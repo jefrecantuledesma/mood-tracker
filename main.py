a48493232422aa8e25f49ddf3fe63ccd634efe48
jefrecantuledesma <98501869+jefrecantuledesma@users.noreply.github.com>
2023-12-13 02:43:29 -0500

    Minor revisions
    Added "importance" feature/flag. Tomorrow, I will make graphs.

--- main.py
+++ main.py
@@ -8,14 +8,16 @@ def cli_parse():
 
     parser.add_argument("-e", "--enter", action="store_true", help="Input your daily mood.")
     parser.add_argument("-l", "--late", action="store_true", help="Use if you are submitting past 11:59pm.")
+    parser.add_argument("-i", "--important", action="store_true", help="Mark as an important day.")
 
     args = parser.parse_args()
 
     if args.enter:
         late = args.late
+        importance = args.important
         mood = get_mood()
         desc = get_desc()
-        write_mood(mood, late, desc)
+        write_mood(mood, late, desc, importance)
 
 def get_desc() -> str:
     return input("Please input a short description of your mood throughout the day: ")
@@ -34,31 +36,35 @@ def get_mood() -> int:
         return mood_int
 
 def exists(input_date: str, sheet) -> bool:
-    #rows = sheet.number_of_rows()
     if int(sheet.number_of_rows()) >= 2:
         last_row = sheet.column[0][-1]
         second_last_row = sheet.column[0][-2]
-        if last_row or second_last_row == input_date:
+        if last_row == input_date:
+            return True
+        elif second_last_row == input_date:
             return True
         else: 
             return False
     else:
         return False
 
-def write_mood(mood: int, late: bool, desc: str):
+def write_mood(mood: int, late: bool, desc: str, importance: bool):
     if not late:
         input_date = date.today()
     else:
         input_date = date.today() - timedelta(days = 1)
 
+    print(input_date, late)
+
     file = "/home/fribbit/Documents/personal_documents/journaling/mood_tracker.ods"
 
-    data = [str(input_date), str(mood), desc]
+    data = [str(input_date), str(mood), importance, desc]
 
     sheet = pe.get_sheet(file_name=file)
+    print(exists(str(input_date), sheet))
     if exists(str(input_date), sheet):
         print("You've already entered data.")
-        return "Penis"
+        exit()
 
     sheet.row += data
     sheet.save_as(file)
